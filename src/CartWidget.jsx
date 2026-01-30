41089b64eae851af7edce3bf74337392299aa2a8
Carlos Aguirre <carlosanibal815@gmail.com>
2026-01-30 11:32:30 -0300

    add carrito

---
+++ src/CartWidget.jsx
@@ -0,0 +1,13 @@
+function CartWidget () {
+    
+    return (
+        <div>
+            <img src="./img/cart.png" alt="Carrito de Compras" style={{width:30}} />
+        </div>
+    )
+
+
+
+}
+
+export default CartWidget
